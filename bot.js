d390082caa290600f77bd3a0ca14513066fe2f23
Vlad <142506318+improve11@users.noreply.github.com>
2026-07-13 11:10:57 +0800

    bot_menu

--- bot.js
+++ bot.js
@@ -6,7 +6,27 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет, октагон!');
+    bot.sendMessage(chatId, 'Привет, октагон!!!');
+});
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpText = `
+📋 Список команд:
+/site - отправляет ссылку на сайт Октагона
+/creator - показывает создателя бота
+    `;
+    bot.sendMessage(chatId, helpText);
+});
+
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'https://octagon-students.ru');
+});
+
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Bashirov Ivan Andreevich IC 1-23-2'); 
 });
 
 console.log('Бот запущен и готов к работе!');
